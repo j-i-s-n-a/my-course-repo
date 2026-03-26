b1942bb7023f063d8d9ccf82cdd64b0b72e7cde1
j-i-s-n-a <jisna293@gmail.com>
2026-03-26 08:31:15 -0400

    changes made from the lab environment

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    path('', RedirectView.as_view(url='/onlinecourse/', permanent=False)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
